beab56c899d4ad8b3c801f939682361e56d3684a
ArturHamannRonconi <arturhronconi@gmail.com>
2021-07-08 21:11:50 -0300

    add tsconfig-paths

--- jest.config.ts
+++ jest.config.ts
@@ -12,4 +12,11 @@ export default {
   testMatch: [
     'src/modules*.spec.ts',
   ],
+  moduleNameMapper: {
+    '@app/(.*)': '<rootDir>/src/$1',
+    '@appTypes/(.*)': ['<rootDir>/src/@types/appTypes/$1'],
+    '@config/(.*)': ['<rootDir>/src/config/$1'],
+    '@accounts/(.*)': ['<rootDir>/src/modules/accounts/$1'],
+    '@utils/(.*)': ['<rootDir>/src/utils/$1']
+  }
 }
