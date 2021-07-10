7a0bdab02c41b00ef507415e91f11c2675bc82c5
ArturHamannRonconi <arturhronconi@gmail.com>
2021-07-09 23:16:45 -0300

    add jest setup

--- jest.config.ts
+++ jest.config.ts
@@ -2,11 +2,20 @@ export default {
   clearMocks: true,
   preset: 'ts-jest',
   coverageProvider: 'v8',
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text-summary'],
+  setupFiles: ['<rootDir>/jest.setup.ts'],
+
+  collectCoverageFrom: [
+    '<rootDir>/src/modules/**/services/**/*.ts',
+    '<rootDir>/src/modules/**/controllers/**/*.ts'
+  ],
   testMatch: ['**/*.spec.ts'],
   moduleNameMapper: {
     '@app/(.*)': '<rootDir>/src/$1',
     '@appTypes/(.*)': ['<rootDir>/src/@types/appTypes/$1'],
     '@config/(.*)': ['<rootDir>/src/config/$1'],
+    '@errors/(.*)': ['<rootDir>/src/errors/$1'],
     '@accounts/(.*)': ['<rootDir>/src/modules/accounts/$1'],
     '@utils/(.*)': ['<rootDir>/src/utils/$1']
   }
