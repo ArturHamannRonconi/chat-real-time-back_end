0f546d70a3602e3e576350c11c49bba21d894d16
ArturHamannRonconi <arturhronconi@gmail.com>
2021-07-14 14:54:10 -0300

    create Tokens migration

---
+++ src/shared/infra/database/migrations/1626284606835-CreateTokens.ts
@@ -0,0 +1,34 @@
+import {MigrationInterface, QueryRunner, Table} from 'typeorm'
+
+export class CreateTokens1626284606835 implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void>
+  {
+    await queryRunner.createTable(
+      new Table({
+        name: 'tokens',
+        columns: [
+          { name: 'id', type: 'uuid', isPrimary: true, isGenerated: true, generationStrategy: 'uuid' },
+          { name: 'user_id', type: 'uuid' },
+          { name: 'token', type: 'varchar' },
+          { name: 'expires_date', type: 'timestamp' },
+          { name: 'created_at', type: 'timestamp', default: 'NOW()' },
+          { name: 'updated_at', type: 'timestamp', default: 'NOW()' }
+        ],
+        foreignKeys: [
+          {
+            name: 'fk_tokens_user_id',
+            columnNames: ['user_id'],
+            referencedTableName: 'users',
+            referencedColumnNames: ['id']
+          }
+        ]
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void>
+  {
+    await queryRunner.dropTable('tokens')
+  }
+}
