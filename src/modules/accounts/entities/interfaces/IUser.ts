3601b209fb436006a547809795594c17051454d9
ArturHamannRonconi <arturhronconi@gmail.com>
2021-07-15 21:57:17 -0300

    add user tokens interface

--- src/modules/accounts/entities/interfaces/IUser.ts
+++ src/modules/accounts/entities/interfaces/IUser.ts
@@ -1,9 +1,12 @@
+import IUserToken from '@accounts/entities/interfaces/IUserToken'
+
 interface IUser
 {
   id: string
   username: string
   email: string
   password: string
+  tokens: IUserToken[]
   updated_at: Date
   created_at: Date
 }
--- src/modules/accounts/entities/interfaces/IUserToken.ts
+++ src/modules/accounts/entities/interfaces/IUserToken.ts
@@ -1,8 +1,11 @@
+import IUser from '@accounts/entities/interfaces/IUser'
+
 interface IUserToken
 {
   id: string
   token: string
   user_id: string
+  user: IUser
   expires_date: Date
   updated_at: Date
   created_at: Date
